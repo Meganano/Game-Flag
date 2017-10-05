73f61a423572ce384ab9724abe4f31726ff693c5
Kittiya Ku-kiattikun <iherep.hiw@hotmail.com>
2017-10-05 20:10:55 +0700

    Add Players,Towers, and flags

--- window.py
+++ window.py
@@ -3,32 +3,39 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 500
 
 class MyApplication(arcade.Window):
-    def __init__(self, width, height):
-        
+    def __init__(self, width, height):       
         # Call the parent class initializer
         super().__init__(width, height)
 
-        # Background image will be stored in this variable
-    ##    self.background = None
-
-        # Set the background color
-        arcade.set_background_color(arcade.color.BLUE)
 
     def setup(self):
         """ Set up the game and initialize the variables. """
         self.background = arcade.load_texture("images/space-bg.jpg")
+        self.player1 = arcade.Sprite('images/Player1.png')
+        self.player2 = arcade.Sprite('images/Player2.png')
+        self.town1 = arcade.Sprite('images/town1.png')
+        self.town2 = arcade.Sprite('images/town2.png')
+        self.flag1 = arcade.Sprite('images/flag1.png')
+        self.flag2 = arcade.Sprite('images/flag2.png')
+        self.player1.set_position(150,250)
+        self.player2.set_position(650,250)
+        self.town1.set_position(50,270)
+        self.town2.set_position(750,270)
+        self.flag1.set_position(50,270)
+        self.flag2.set_position(750,270)
 
     def on_draw(self):
-        # This command has to happen before we start drawing
         arcade.start_render()
-
         # Draw the background texture
-        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
-                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.player1.draw()
+        self.player2.draw()
+        self.town1.draw()
+        self.town2.draw()
+        self.flag1.draw()
+        self.flag2.draw()
 
 def main():
-    """ Main method """
     window = MyApplication(SCREEN_WIDTH, SCREEN_HEIGHT)
     window.setup()
     arcade.run()
