8a9f5788e43d55356e525875c95356abf21d79db
Kittiya Ku-kiattikun <iherep.hiw@hotmail.com>
2017-10-05 22:53:03 +0700

    Split player1 class

--- window.py
+++ window.py
@@ -1,4 +1,6 @@
 import arcade
+from firstplayer import Player1
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 500
 
@@ -11,13 +13,17 @@ class MyApplication(arcade.Window):
     def setup(self):
         """ Set up the game and initialize the variables. """
         self.background = arcade.load_texture("images/space-bg.jpg")
-        self.player1 = arcade.Sprite('images/Player1.png')
+
+        self.player1 = Player1(150,250)
+        self.player1_model = arcade.Sprite('images/Player1.png')
+        #self.player1.set_position(150,250)
+        
         self.player2 = arcade.Sprite('images/Player2.png')
         self.town1 = arcade.Sprite('images/town1.png')
         self.town2 = arcade.Sprite('images/town2.png')
         self.flag1 = arcade.Sprite('images/flag1.png')
         self.flag2 = arcade.Sprite('images/flag2.png')
-        self.player1.set_position(150,250)
+        
         self.player2.set_position(650,250)
         self.town1.set_position(50,270)
         self.town2.set_position(750,270)
@@ -26,9 +32,10 @@ class MyApplication(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        # Draw the background texture
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-        self.player1.draw()
+
+        self.player1_model.draw()
+        
         self.player2.draw()
         self.town1.draw()
         self.town2.draw()
@@ -37,10 +44,9 @@ class MyApplication(arcade.Window):
         
     def update(self, delta):
         player1 = self.player1
- 
-        if player1.center_y > SCREEN_HEIGHT:
-            player1.center_y = SCREEN_HEIGHT
-        player1.set_position(player1.center_x, player1.center_y + 5)
+
+        player1.update(delta) 
+        self.player1_model.set_position(player1.x, player1.y + 5)
 
 
 
